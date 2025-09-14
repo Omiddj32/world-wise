32c9ab6f6c26ec3d3cf218dec4ae7161e8f13ae3
Omid Jahangiri <omiddj9909@gmail.com>
2025-09-14 12:41:39 +0330

    Add files via upload

--- contexts/CitiesContext.jsx
+++ contexts/CitiesContext.jsx
@@ -2,7 +2,8 @@ import { createContext, useEffect, useContext, useReducer } from "react";
 
 const CitiesContext = createContext();
 
-const BASE_URL = "https://world-wise-b7cnrjxxd-omiddj32s-projects.vercel.app";
+// const BASE_URL = "http://localhost:9000";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const initialState = {
   cities: [],
